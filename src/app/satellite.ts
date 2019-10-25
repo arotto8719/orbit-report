c3c70fc372572e7b46e0690b0e68a900256251f8
arotto8719 <arotto87@gmail.com>
2019-10-25 15:47:05 -0500

    updated Angular

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -4,6 +4,13 @@ orbitType: string;
 type: string;
 operational: boolean;
 launchDate: string;
+
+    constructor() {
+    name: string;
+type: string;
+launchDate: string;
+orbitType: string;
+operational: boolean;
+    }
 }
 
-constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean)
